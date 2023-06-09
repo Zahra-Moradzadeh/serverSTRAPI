c8b4da3828907c86dcf136a251062e33b380e30c
mzahra976 <moradzade976@gmail.com>
2023-06-09 22:41:10 +0330

    make

---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+const crypto = require("crypto");
+
+module.exports = ({ env }) => ({
+
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET") || crypto.randomBytes(16).toString("base64"),
+    },
+  },
+});
